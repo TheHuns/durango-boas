81a8dcec67234554c5bffc8b548790d7e91a9a67
Dehn Hunsworth <dhunsworth@yahoo.com>
2020-02-04 16:56:24 -0700

    cms & added pic

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,9 +4,11 @@ import cloudinary from "netlify-cms-media-library-cloudinary";
 
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import AvailablePostPreview from "./preview-templates/AvailablePostPreview";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("available", AvailablePostPreview);
